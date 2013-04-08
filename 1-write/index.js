ea9224a6914afa725de2c825bc2ad5ef3a0cd5f1
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-04-09 00:15:24 +0300

    Update index.js

--- 1-write/index.js
+++ 1-write/index.js
@@ -1,8 +1,9 @@
-
 // http://nodejs.org/api/stream.html#stream_event_end
 // http://nodejs.org/api/stream.html#stream_writable_end_chunk_encoding_callback
 
 /**
+ * Writes "hello world" to the stream
+ * 
  * @param {stream.Writable}
  **/
 module.exports = function(writeable) {
