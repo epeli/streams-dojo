098166265479d328c360a09bc7bd82a7f5c6909d
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-04-09 10:12:32 +0300

    Update index.js

--- 3-concat/index.js
+++ 3-concat/index.js
@@ -1,8 +1,9 @@
-
 // http://nodejs.org/api/stream.html#stream_readable_read_size_1
 // http://nodejs.org/api/stream.html#stream_event_end
 
 /**
+ * Read the readable stream to a single string and return it with the callback
+ * 
  * @param {stream.Readable}
  * @param {Function} node.js style callback to be called with concatenated
  * string: cb(null, string);
